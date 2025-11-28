4a5433c83e89d62840e00617bc1dda7daa933d03
TngocNhut <tranngocnhat2106@gmail.com>
2025-11-28 16:32:06 +0700

    Refactor Category class with new fields and methods

--- app/src/main/java/vn/hcmute/eatandorder/data/model/Category.java
+++ app/src/main/java/vn/hcmute/eatandorder/data/model/Category.java
@@ -1,15 +1,70 @@
 package vn.hcmute.eatandorder.data.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Category {
-    private Long id;
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("name")
     private String name;
-    private String imageUrl;   // có thể null
 
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getImageUrl() { return imageUrl; }
+    @SerializedName("image")
+    private String image;
 
-    public void setId(Long id) { this.id = id; }
-    public void setName(String name) { this.name = name; }
-    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+    @SerializedName("description")
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(int id, String name, String image, String description) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
